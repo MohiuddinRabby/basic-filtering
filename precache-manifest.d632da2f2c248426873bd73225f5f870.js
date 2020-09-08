50d5ad33ba72e201be9f88fb2db8b4eff3d87d1c
rabbymohiuddin <rabbymohiuddin@gmail.com>
2020-09-08 09:56:16 +0600

    Updates

---
+++ precache-manifest.d632da2f2c248426873bd73225f5f870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a12d087bfa5b99c12bcf704cb55ffe",
+    "url": "/basic-filtering/index.html"
+  },
+  {
+    "revision": "92e73e21ed05cf183a8c",
+    "url": "/basic-filtering/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "92e73e21ed05cf183a8c",
+    "url": "/basic-filtering/static/js/2.85ba2a06.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/basic-filtering/static/js/2.85ba2a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cb568f4af5d99687333",
+    "url": "/basic-filtering/static/js/main.da73e44b.chunk.js"
+  },
+  {
+    "revision": "766964df064b6a2f3f07",
+    "url": "/basic-filtering/static/js/runtime-main.6ed69f6c.js"
+  }
+]);
